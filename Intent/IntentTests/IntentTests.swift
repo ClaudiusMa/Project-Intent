0e3417ba0d4d6bc9edf252a6da0476795ad63254
Claudius Ma <heiclaudius@gmail.com>
2024-10-14 19:44:28 +0800

    Proejct Setup

---
+++ Intent/IntentTests/IntentTests.swift
@@ -0,0 +1,17 @@
+//
+//  IntentTests.swift
+//  IntentTests
+//
+//  Created by Claudius Ma on 10/14/24.
+//
+
+import Testing
+@testable import Intent
+
+struct IntentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
